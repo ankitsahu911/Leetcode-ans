72a60906c62cd92132a9bdbba973ad1eb5dcd886
ankitsahu911 <ankit.sthitapranjya2024@vitstudent.ac.in>
2026-08-08 23:48:46 +0530

    Time: 0 ms (100%), Space: 71.7 MB (53.62%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+        
+        for (size_t i = 1; i < nums.size(); ++i) {
+            currentMax = max(nums[i], currentMax + nums[i]);
+            maxSoFar = max(maxSoFar, currentMax);
+        }
+        
+        return maxSoFar;
+    }
+};
