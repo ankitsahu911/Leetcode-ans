65787c382ff707867375c2151af41cf7db037f6d
ankitsahu911 <ankit.sthitapranjya2024@vitstudent.ac.in>
2026-09-03 01:09:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (45.21%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return buildTrees(1, n);
+    }
+
+private:
+    vector<TreeNode*> buildTrees(int start, int end) {
+        if (start > end) {
+            return {nullptr};
+        }
+
+        vector<TreeNode*> allTrees;
+
+        for (int i = start; i <= end; ++i) {
+            vector<TreeNode*> leftTrees = buildTrees(start, i - 1);
+            vector<TreeNode*> rightTrees = buildTrees(i + 1, end);
+
+            for (auto left : leftTrees) {
+                for (auto right : rightTrees) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    allTrees.push_back(root);
+                }
+            }
+        }
+
+        return allTrees;
+    }
+};
